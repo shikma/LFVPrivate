663657dbe9d74173953886c0591968ca747cf143
avitaldery <avitaldery@weizmann.ac.il>
2014-01-19 14:10:44 +0200

    EM & ME Mcoll histos

--- macros/Looper.C
+++ macros/Looper.C
@@ -2,15 +2,26 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "TChain.h"
+#include "TCanvas.h"
+#include "TString.h"
+#include "TLorentzVector.h"
 #include "TH1.h"
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
-void Looper(bool useEM)
+void Looper(TString infile,TString outfile, bool useEM, bool useCuts)
 {
+	//CUT VALUES
+	float L0_PT_CUT = 30;
+	float L1_PT_CUT = 10;
+	float dPhi_l0l1_CUT = 0;
+	float dPhi_l1Met_CUT = 5;
+
+
 	TChain* chain = new TChain("t","");
-	chain->Add("../run/Z_noSmearing.root/t");
+	chain->Add("../run/"+infile+"/t");
 	TTree* tree = chain;
 
 	SimData* data = new SimData(tree);
@@ -24,11 +35,39 @@ void Looper(bool useEM)
     Int_t nTen = nentries/10;
     Int_t nbytes = 0, nb = 0;
 
-    TH1D* elPt = new TH1D("elPt","elPt",60,0,300);
-    TH1D* muPt = new TH1D("muPt","muPt",60,0,300);
+    TH1D* h_elPt = new TH1D("elPt","elPt",150,0,300);
+    TH1D* h_muPt = new TH1D("muPt","muPt",150,0,300);
+    TH1D* h_elPt_2 = new TH1D("elPt2","elPt2",150,0,300);
+    TH1D* h_muPt_2 = new TH1D("muPt2","muPt2",150,0,300);
+    TH1D* h_ME_l0_Pt = new TH1D("ME_l0_Pt","ME_l0_Pt",150,0,300);
+    TH1D* h_EM_l0_Pt = new TH1D("EM_l0_Pt","EM_l0_Pt",150,0,300);
+    TH1D* h_ME_l1_Pt = new TH1D("ME_l1_Pt","ME_l1_Pt",150,0,300);
+    TH1D* h_EM_l1_Pt = new TH1D("EM_l1_Pt","EM_l1_Pt",150,0,300);
+    TH1D* h_ME_dPhi_l0_l1 = new TH1D("ME_dPhi_l0_l1","ME_dPhi_l0_l1",50,0,5);
+    TH1D* h_EM_dPhi_l0_l1 = new TH1D("EM_dPhi_l0_l1","EM_dPhi_l0_l1",50,0,5);
+    TH1D* h_ME_dPhi_l1_MET = new TH1D("ME_dPhi_l1_MET","ME_dPhi_l1_MET",50,0,5);
+    TH1D* h_EM_dPhi_l1_MET = new TH1D("EM_dPhi_l1_MET","EM_dPhi_l1_MET",50,0,5);
+    TH1D* h_EM_dEta_l0_l1 = new TH1D("EM delta Eta","EM delta Eta",50,0,5);
+    TH1D* h_ME_dEta_l0_l1 = new TH1D("ME delta Eta","ME delta Eta",50,0,5);
+    TH1D* h_EM_Mcoll = new TH1D("EM_Mcoll","EM_Mcoll",250,0,500);
+    TH1D* h_ME_Mcoll = new TH1D("ME_Mcoll","ME_Mcoll",250,0,500);
+    TH1D* h_eleta = new TH1D("elEta","elEta",60,-3,3);
+    TH1D* h_mueta = new TH1D("muEta","muEta",60,-3,3);
 
-    TH1D* eleta = new TH1D("elEta","elEta",60,-3,3);
-    TH1D* mueta = new TH1D("muEta","muEta",60,-3,3);
+    TH1D* h_delta = new TH1D("elPt-muPt","elPt-muPt",150,0,300);
+
+
+
+    //output
+    TFile* file = new TFile(outfile,"RECREATE");
+
+    //cut flow counters
+    int n_readIn = 0;
+    int n_pass2DifferentFlavor = 0;
+    int n_passOppositeSign = 0;
+    int n_passL0Pt = 0;
+    int n_passL1Pt = 0;
+
 
 
     for (Int_t jentry=0; jentry<nentries; jentry++)
@@ -39,6 +78,7 @@ void Looper(bool useEM)
 		nb = data->fChain->GetEntry(jentry);
 		nbytes += nb;
 
+		n_readIn++;
 		if (nTen!=0) if (jentry%nTen==0) { cout << " " << 10*(jentry/nTen) << "%-" <<flush;                }
 
 //	 	printf("evt %d: nPhoton %d, nElectron %d, nMuon %d, nTau %d, nJet %d \n",
@@ -47,7 +87,67 @@ void Looper(bool useEM)
 	 	int n_e = data->nElectron;
 	 	int n_m = data->nMuon;
 
-	  	if (useEM &&(n_e!=1 || n_m!=1)){continue; 	}
+	  	if (useEM) {
+	  		if (n_e!=1 || n_m!=1){continue; 	}
+	  		n_pass2DifferentFlavor++;
+
+	  		//create TLorentzVectors
+	  		float ElPhi = data->electronPhi[0];
+	  		float ElPt = data->electronPt[0];
+	  		TLorentzVector vEle;
+	  		vEle.SetPxPyPzE(ElPt*cos(ElPhi),ElPt*sin(ElPhi),0,ElPt);
+	  		float MuPhi = data->muonPhi[0];
+	  		float MuPt = data->muonPt[0];
+	  		TLorentzVector vMu;
+	  		vMu.SetPxPyPzE(MuPt*cos(MuPhi),MuPt*sin(MuPhi),0,MuPt);
+	  		float MetPhi = data->metPhi;
+	  		float MetPt = data->metPt;
+	  		TLorentzVector vMet;
+	  		vMet.SetPxPyPzE(MetPt*cos(MetPhi),MetPt*sin(MetPhi),0,MetPt);
+
+	  		if (useCuts){
+	  			//Opposite Sign
+	  			if (data->electronSign[0]==data->muonSign[0]){continue; }
+	  			n_passOppositeSign++;
+	  			//L0 Pt
+	  			if ((ElPt >= MuPt)&&(ElPt < L0_PT_CUT)){continue; }
+	  			if ((ElPt < MuPt)&&(MuPt < L0_PT_CUT)){continue; }
+	  			n_passL0Pt++;
+	  			//L1 Pt
+	  			if ((ElPt >= MuPt)&&(MuPt < L1_PT_CUT)){continue; }
+	  			if ((ElPt < MuPt)&&(ElPt < L1_PT_CUT)){continue; }
+	  			n_passL1Pt++;
+	  		}
+
+
+
+			h_elPt_2->Fill(ElPt);
+	  		h_muPt_2->Fill(MuPt);
+
+	  		if (ElPt >= MuPt){
+	  			h_EM_l0_Pt->Fill(ElPt);
+	  			h_EM_l1_Pt->Fill(MuPt);
+	  			double dPhiElMu = vEle.DeltaPhi(vMu);
+	  			h_EM_dPhi_l0_l1->Fill(abs(dPhiElMu));
+	  			h_EM_dPhi_l1_MET->Fill(abs(vMu.DeltaPhi(vMet)));
+	  			double dEtaElMu = abs(data->electronEta[0]-data->muonEta[0]);
+	  			h_EM_dEta_l0_l1->Fill(abs(dEtaElMu));
+	  			double Mcoll = sqrt(2.0*ElPt*(MuPt+MetPt)*(cosh(dEtaElMu) - cos(dPhiElMu)));
+//	  			cout<<"MEt  ="<<MetPt<<"cosh = "<<cosh(dEtaElMu)<<",cos = "<<cos(dPhiElMu)<<",Mcoll = "<<Mcoll<<endl;
+	  			h_EM_Mcoll->Fill(Mcoll);
+	  		}
+	  		else{
+	  			h_ME_l1_Pt->Fill(ElPt);
+	  			h_ME_l0_Pt->Fill(MuPt);
+	  			double dPhiElMu = vEle.DeltaPhi(vMu);
+	  			h_ME_dPhi_l0_l1->Fill(abs(dPhiElMu));
+	  			h_ME_dPhi_l1_MET->Fill(abs(vEle.DeltaPhi(vMet)));
+	  			double dEtaElMu = data->electronEta[0]-data->muonEta[0];
+	  			h_ME_dEta_l0_l1->Fill(abs(dEtaElMu));
+	  			double Mcoll = sqrt(2.0*MuPt*(ElPt+MetPt)*(cosh(dEtaElMu) - cos(dPhiElMu)));
+	  			h_ME_Mcoll->Fill(Mcoll);
+	  		}
+	  	}
 
 	 	if (n_e>0)
 	 	{
@@ -59,14 +159,13 @@ void Looper(bool useEM)
 	 					//(data->electronTrk02[i] < 0.2*data->electronPt[i]))
 	 					//data->electronCal04[i] < 0.1*data->electronPt[i])
 	 			{
-	 				elPt->Fill(data->electronPt[i]);
-	 				eleta->Fill(data->electronEta[i]);
+	 				h_elPt->Fill(data->electronPt[i]);
+	 				h_eleta->Fill(data->electronEta[i]);
 	 				if(data->electronPt[i] < 0)
 	 				{
 //	 					cout<<"electron# "<<i+1<<" out of "<< n_e<<" pt "<<data->electronPt[i]<<endl;
 	 				}
 	 			}
-
 	 		}
 	 	}
 	 	if (n_m>0)
@@ -79,24 +178,89 @@ void Looper(bool useEM)
 	 					//(data->muonTrk02[i] < 0.2*data->muonPt[i]))
 	 					//data->muonCal04[i] < 0.1*data->muonPt[i])
 	 			{
-	 				muPt->Fill(data->muonPt[i]);
-	 				mueta->Fill(data->muonEta[i]);
+	 				h_muPt->Fill(data->muonPt[i]);
+	 				h_mueta->Fill(data->muonEta[i]);
 //	 				cout<<"muon# "<<i+1<<" out of "<< n_m<<" pt "<<data->muonPt[i]<<endl;
 	 			}
-
 	 		}
 	 	}
-
 	}
 
-    elPt->SetLineColor(kGreen+2);
-    elPt->Draw();
-    muPt->Draw("sames");
+    if (useEM){
+    	TCanvas *c1 = new TCanvas("leading Lepton Pt","leading Lepton Pt",600,400);	c1->cd();
+    	h_EM_l0_Pt->SetLineColor(kGreen+2); h_EM_l0_Pt->SetLineWidth(2); h_EM_l0_Pt->Draw("E1");
+    	h_ME_l0_Pt->SetLineWidth(2); h_ME_l0_Pt->Draw("sames E1");
+       	TCanvas *c2 = new TCanvas("subleading Lepton Pt","subleading Lepton Pt",600,400); c2->cd();
+       	h_EM_l1_Pt->SetLineColor(kGreen+2); h_EM_l1_Pt->SetLineWidth(2); h_EM_l1_Pt->Draw("E1");
+       	h_ME_l1_Pt->SetLineWidth(2); h_ME_l1_Pt->Draw("sames E1");
+    	TCanvas *c3 = new TCanvas("Electron Muon Pt","Electron Muon Pt",600,400); c3->cd();
+    	h_elPt_2->SetLineColor(kGreen+2); h_elPt_2->SetLineWidth(2); h_elPt_2->Draw("E1");
+    	h_muPt_2->SetLineWidth(2); h_muPt_2->Draw("sames E1");
+    	TCanvas *c4 = new TCanvas("DeltaPhi(el,mu)","DeltaPhi(el,mu)",600,400); c4->cd();
+    	h_EM_dPhi_l0_l1->SetLineColor(kGreen+2); h_EM_dPhi_l0_l1->SetLineWidth(2); h_EM_dPhi_l0_l1->Draw("E1");
+    	h_ME_dPhi_l0_l1->SetLineWidth(2); h_ME_dPhi_l0_l1->Draw("sames E1");
+    	TCanvas *c5 = new TCanvas("DeltaPhi(l1,met)","DeltaPhi(l1,met)",600,400); c5->cd();
+    	h_EM_dPhi_l1_MET->SetLineColor(kGreen+2); h_EM_dPhi_l1_MET->SetLineWidth(2); h_EM_dPhi_l1_MET->Draw("E1");
+    	h_ME_dPhi_l1_MET->SetLineWidth(2); h_ME_dPhi_l1_MET->Draw("sames E1");
+    	TCanvas *c6 = new TCanvas("c6","c6",600,400); c6->cd();
+    	TH1D* h_elPtMinusMuPt = (TH1D*)h_elPt_2->Clone("elPtMinusMuPt");
+    	TH1D* h_ptSum = (TH1D*)h_elPt_2->Clone("sum");
+    	h_ptSum->Add(h_muPt_2,1);
+    	h_elPtMinusMuPt->Add(h_muPt_2,-1);
+    	h_delta->Divide(h_elPtMinusMuPt,h_ptSum,1,2);
+    	h_delta->Draw("E1");
+    	TCanvas *c7 = new TCanvas("DeltaEta(l0,l1)","DeltaEta(l0,l1)",600,400); c7->cd();
+    	h_ME_dEta_l0_l1->SetLineWidth(2); h_ME_dEta_l0_l1->Draw("E1");
+    	h_EM_dEta_l0_l1->SetLineColor(kGreen+2); h_EM_dEta_l0_l1->SetLineWidth(2); h_EM_dEta_l0_l1->Draw("sames E1");
+    	TCanvas *c8 = new TCanvas("Mcoll","Mcoll",600,400); c8->cd();
+    	h_EM_Mcoll->SetLineWidth(2); h_EM_Mcoll->SetLineColor(kGreen+2); h_EM_Mcoll->Draw("E1");
+    	h_ME_Mcoll->SetLineWidth(2); h_ME_Mcoll->Draw("sames E1");
 
-    eleta->SetLineColor(kGreen+2);
 
-//    eleta->DrawNormalized();
-//    mueta->DrawNormalized("sames");
+
+//    	h_mueta->Draw("E1");
+//    	h_eleta->SetLineColor(kGreen+2);h_eleta->Draw("sames E1");
+//    	h_elPtMinusMuPt->Draw();
+
+    	h_EM_l0_Pt->Write();
+    	h_ME_l0_Pt->Write();
+    	h_EM_l1_Pt->Write();
+    	h_ME_l1_Pt->Write();
+    	h_EM_dPhi_l0_l1->Write();
+    	h_ME_dPhi_l0_l1->Write();
+    	h_EM_dPhi_l1_MET->Write();
+    	h_ME_dPhi_l1_MET->Write();
+    	h_EM_dEta_l0_l1->Write();
+    	h_ME_dEta_l0_l1->Write();
+    	h_EM_Mcoll->Write();
+    	h_ME_Mcoll->Write();
+    	h_elPt_2->Write();
+    	h_muPt_2->Write();
+
+    }
+
+//    h_elPt->Write();
+//    h_muPt->Write();
+
+//    h_elPt->SetLineColor(kGreen+2);
+//    h_elPt->Draw();
+//    h_muPt->Draw("sames");
+
+//    h_eleta->SetLineColor(kGreen+2);
+
+//    h_eleta->DrawNormalized();
+//    h_mueta->DrawNormalized("sames");
+
+    if (useCuts){
+    	cout<<endl;
+		cout<<"# read in: "<<n_readIn<<endl;
+		cout<<"# passed exactly 2 DF: "<<n_pass2DifferentFlavor<<endl;
+		cout<<"# passed Opposite Sign: "<<n_passOppositeSign<<endl;
+		cout<<"# passed l0 Pt cut: "<<n_passL0Pt<<endl;
+		cout<<"# passed l1 Pt cut: "<<n_passL1Pt<<endl;
+    }
+
+    file->Close();
 
     delete data;
 }
