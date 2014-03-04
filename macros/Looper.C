46ece2196f11692696798f78ce6c9095c3510183
avitaldery <avitaldery@weizmann.ac.il>
2014-03-04 20:12:02 +0200

    cutflow up to dPhi(leptons)

--- macros/Looper.C
+++ macros/Looper.C
@@ -18,7 +18,7 @@ void Looper(TString infile,TString outfile, bool useEM, bool useCuts)
 	//CUT VALUES
 	float L0_PT_CUT = 30;
 	float L1_PT_CUT = 10;
-	float dPhi_l0l1_CUT = 0;
+	float dPhi_l0l1_CUT = 2.5;
 	float dPhi_l1Met_CUT = 5;
 
 
