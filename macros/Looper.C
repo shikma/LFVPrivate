df474a0463e8a5606fc797ceb05cdc22c1f9ea0e
avitaldery <avitaldery@weizmann.ac.il>
2014-04-09 15:54:49 +0300

    lower l1 pt cut to 10

--- macros/Looper.C
+++ macros/Looper.C
@@ -17,7 +17,7 @@ void Looper(TString infile,TString outfile, bool useEM, bool useCuts)
 {
 	//CUT VALUES
 	float L0_PT_CUT = 30;
-	float L1_PT_CUT = 20;
+	float L1_PT_CUT = 10;
 	float dPhi_l0l1_CUT = 2.5;
 	float dPhi_l1Met_CUT = 0.5;
 
