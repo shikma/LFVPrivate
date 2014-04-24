431ca5dda09eb6442ab16006ae4171759e78712d
avitaldery <avitaldery@weizmann.ac.il>
2014-04-24 11:37:26 +0300

    detector stuff

---
+++ LFVPrivate/DetectorData.h
@@ -0,0 +1,7 @@
+#include "TString.h"
+
+namespace DetectorData {
+float EfficiencyForParticle(TString particle,float Pt, float eta, float phi);
+float DeviationForParameter(TString parameterName, float parameterValue);
+
+}
--- cmt/LFVPrivate.C
+++ cmt/LFVPrivate.C
@@ -1,3 +1,4 @@
 //classes
 #include "Root/SimData.cxx"
 #include "Root/Tools.cxx"
+#include "Root/DetectorData.cxx"
